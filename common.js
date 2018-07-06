4a03ab9987081514b7489150b95491098d68fb0e
Bai, Tony <tony.bai01@sap.com>
2018-07-06 18:00:42 +0800

    Prepare to weibo project

---
+++ common.js
@@ -0,0 +1,12 @@
+let commonMod = angular.module('common', []);
+commonMod.config(function($httpProvider) {
+    $httpProvider.defaults.transformRequest = function(obj) {
+        let arr = [];
+        for (let name in obj) {
+            arr.push(`${name}=${obj[name]}`);
+        }
+        return arr.join("&");
+    };
+
+    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+});
